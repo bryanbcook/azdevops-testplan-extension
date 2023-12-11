5b04542fa7d8d11bb42ebe5353135c1307a7861b
bryan cook <3217452+bryanbcook@users.noreply.github.com>
2023-12-11 18:44:10 -0500

    Add support for additional test frameworks as picklist

--- PublishTestPlanResultsV1/framework/TestFrameworkParameters.ts
+++ PublishTestPlanResultsV1/framework/TestFrameworkParameters.ts
@@ -8,5 +8,11 @@ export class TestFrameworkParameters {
   constructor(files: string[], format: string) {
     this.testFiles = files;
     this.testFormat = TestFrameworkFormat[format as keyof typeof TestFrameworkFormat];
+
+    if (this.testFormat === undefined) {
+      let keys : string[] = Object.keys(TestFrameworkFormat);
+
+      throw new Error(`testResultformat '${format}' is not supported. Please specify one of the following values: ${ keys.join(', ')}`);
+    }
   }
 }
--- PublishTestPlanResultsV1/test/TestFrameworkResultReader.specs.ts
+++ PublishTestPlanResultsV1/test/TestFrameworkResultReader.specs.ts
@@ -5,34 +5,105 @@ import * as TestFrameworkResultReader from '../framework/TestFrameworkResultRead
 
 describe("TestFramework Results Reader", () => {
 
-  context("xUnit", () => {
+  var baseDir : string;
 
-    var baseDir : string;
+  before(() => {
+    baseDir = path.join(__dirname, "data");
+  })
 
-    before(() => {
-      baseDir = path.join(__dirname, "data");
-    })
+  it("Can read xUnit results", async () => {
+    // arrange
+    var files = [];
+    files.push(path.join(baseDir, "xunit", "xunit-1.xml"));
+    var parameters = new TestFrameworkParameters(files, "xunit");
+    
+    // act
+    var results = await TestFrameworkResultReader.readResults(parameters);
 
-    it("Can read xUnit results", async () => {
-      // arrange
-      var files = [];
-      files.push(path.join(baseDir, "xunit", "xunit-1.xml"));
-      var parameters = new TestFrameworkParameters(files, "xUnit");
-      
-      // act
-      var results = await TestFrameworkResultReader.readResults(parameters);
+    // assert
+    expect(results.length).to.eq(1);
+  });
+ 
+ 
+  it("Can read jUnit results", async () => {
+    // arrange
+    var files = [];
+    files.push(path.join(baseDir, "junit", "single-suite.xml"));
+    var parameters = new TestFrameworkParameters(files, "junit");
+    
+    // act
+    var results = await TestFrameworkResultReader.readResults(parameters);
 
-      // assert
-      expect(results.length).to.eq(1);
-    });
-
+    // assert
+    expect(results.length).to.eq(1);
   });
 
-  // context("jUnit", () => {
+  it("Can read Cucumber results", async () => {
+    // arrange
+    var files = [];
+    files.push(path.join(baseDir, "cucumber", "single-suite-single-test.json"));
+    var parameters = new TestFrameworkParameters(files, "cucumber");
+    
+    // act
+    var results = await TestFrameworkResultReader.readResults(parameters);
 
-  //   it("Can read jUnit results", () => {
-  //     throw new Error("Not implemented");
-  //   });
+    // assert
+    expect(results.length).to.eq(1);
+  });
 
-  // })
+  it("Can read Mocha results", async () => {
+    // arrange
+    var files = [];
+    files.push(path.join(baseDir, "mocha", "single-suite-single-test.json"));
+    var parameters = new TestFrameworkParameters(files, "mocha");
+    
+    // act
+    var results = await TestFrameworkResultReader.readResults(parameters);
+
+    // assert
+    expect(results.length).to.eq(1);
+  });
+
+  it("Can read NUnit results", async () => {
+    // arrange
+    var files = [];
+    files.push(path.join(baseDir, "nunit", "nunit_v3.xml"));
+    var parameters = new TestFrameworkParameters(files, "nunit");
+    
+    // act
+    var results = await TestFrameworkResultReader.readResults(parameters);
+
+    // assert
+    expect(results.length).to.be.greaterThan(0);
+  });
+
+  it("Can read TestNG results", async () => {
+    // arrange
+    var files = [];
+    files.push(path.join(baseDir, "testng", "single-suite.xml"));
+    var parameters = new TestFrameworkParameters(files, "testng");
+    
+    // act
+    var results = await TestFrameworkResultReader.readResults(parameters);
+
+    // assert
+    expect(results.length).to.be.greaterThan(1);
+  });
+
+
+  // waiting for 0.1.7 that includes this
+  // it("Can read MStests results", async () => {
+  //   // arrange
+  //   var files = [];
+  //   files.push(path.join(baseDir, "mstest", "testresults.trx"));
+  //   var parameters = new TestFrameworkParameters(files, "mstest");
+    
+  //   // act
+  //   var results = await TestFrameworkResultReader.readResults(parameters);
+
+  //   // assert
+  //   expect(results.length).to.eq(1);
+  // });
+
+
 })
