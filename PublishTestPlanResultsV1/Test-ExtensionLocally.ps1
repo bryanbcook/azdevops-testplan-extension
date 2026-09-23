d8a3a1743006d898a3df492fcef900a7202ffe30
Bryan Cook <3217452+bryanbcook@users.noreply.github.com>
2026-09-23 12:23:44 -0400

    fix: mstest property support (#159)
    * Introduce regression test for #158
    Resolves #158
    
    * Additional logic in regression tests to report failures
    
    * Additional logic in regression tests to report failures
    
    * removed backtick
    
    * update test-parser to 0.4.7
    
    * we're only interested in test parsing, not matching tests
    
    * fix for regression test config
    
    ---------
    
    Co-authored-by: bryan cook <3217452+bryancook@users.noreply.github.com>

--- PublishTestPlanResultsV1/Test-ExtensionLocally.ps1
+++ PublishTestPlanResultsV1/Test-ExtensionLocally.ps1
@@ -190,10 +190,12 @@ if ($DebugMode.IsPresent) {
 
   $expectedFailures = $failTaskOnFailedTests -eq "true" -or $failTaskOnSkippedTests -eq "true"
   
+  $taskFailurePattern = "##vso\[task\.complete result=Failed;\].*"
+  $taskFailureFound = $outputString -match $taskFailurePattern
+
   if ($expectedFailures -eq "true") {
     # Check if the task explicitly set a failed result via Azure DevOps logging command
-    $taskFailurePattern = "##vso\[task\.complete result=Failed;\].*"
-    $taskFailureFound = $outputString -match $taskFailurePattern
+    
     
     if ($taskFailureFound) {
       Write-Host "Task failure was detected via Azure DevOps logging command. This was expected."
@@ -203,5 +205,8 @@ if ($DebugMode.IsPresent) {
       Write-Host "Task was expected to fail if test failed. No failure was reported."
       Write-Host "##vso[task.issue type=error]Task was expected to fail if test failed. However, no failure was reported."
     }
+  } elseif ($taskFailureFound) {
+    Write-Host "Task was not expected to fail, but a failure was reported."
+    Write-Host "##vso[task.issue type=error]Task was not expected to fail, but a failure was reported."
   }
 }
